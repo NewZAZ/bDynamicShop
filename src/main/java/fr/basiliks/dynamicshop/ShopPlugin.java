2a99371ae8e10aae1b129e192dbe02b5b2dac070
NewZAZ <ben.tintin.bp@gmail.com>
2021-08-28 14:07:51 +0200

    test Commit

---
+++ src/main/java/fr/basiliks/dynamicshop/ShopPlugin.java
@@ -0,0 +1,11 @@
+package fr.basiliks.dynamicshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShopPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
