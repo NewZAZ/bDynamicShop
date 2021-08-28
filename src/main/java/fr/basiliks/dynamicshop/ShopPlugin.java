6bb219cbd5e36c18ea22caedb6825f070b608279
NewZAZ <ben.tintin.bp@gmail.com>
2021-08-28 19:36:39 +0200

    implements nCore ✨

--- src/main/java/fr/basiliks/dynamicshop/ShopPlugin.java
+++ src/main/java/fr/basiliks/dynamicshop/ShopPlugin.java
@@ -1,11 +1,21 @@
 package fr.basiliks.dynamicshop;
 
+import fr.basiliks.dynamicshop.commands.CommandManager;
+import fr.newzproject.core.NCore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShopPlugin extends JavaPlugin {
 
+    private static ShopPlugin instance;
+
+    public static ShopPlugin get() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
-        
+        NCore.register(this);
+        CommandManager.register(this);
+        instance = this;
     }
 }
